cf46090f755c735058ee55c49b9c97f85dd6e3eb
Eddie Riv <eddierivs91@gmail.com>
2016-06-30 22:36:22 +0000

    change navbar to have all categories and admin priviledge for creating category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: 'John', email: 'John@example.com', 
+                        password: 'password', admin: true)
+  end
+  
   test 'get new categories form and create category' do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do 
@@ -13,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test 'invalid category submission results in failure' do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do 
@@ -22,5 +29,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
+
+  
 
 end
